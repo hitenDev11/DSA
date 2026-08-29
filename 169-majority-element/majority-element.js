6b1e8f7820560f4a243565a68050ed36bbb9cd31
Hiten Moriya <hitenmoriya2006@gmail.com>
2026-08-29 14:26:45 +0530

    Time: 1 ms (88.68%) | Memory: 57.8 MB (39.19%) - LeetSync

---
+++ 169-majority-element/majority-element.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let ans = nums[0]
+    let count = 1;
+
+    for(let i = 1;i<nums.length;i++){
+        if(count === 0){
+            ans = nums[i]
+            count = 1
+        }else if(nums[i] === ans) count++
+        else count --
+    }
+
+    return ans
+};
