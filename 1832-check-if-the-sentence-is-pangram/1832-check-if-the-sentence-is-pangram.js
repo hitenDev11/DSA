091df53dd0387a9e81c65918759e302f6791c89f
Hiten Moriya <hitenmoriya2006@gmail.com>
2026-09-05 18:29:38 +0530

    Time: 1 ms (82.52%), Space: 53.3 MB (90.44%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} sentence
+ * @return {boolean}
+ */
+var checkIfPangram = function(sentence) {
+    let set = new Set()
+    for(let i =0;i<sentence.length;i++){
+        let ch = sentence.charAt(i)
+        set.add(ch)
+    }
+     return set.size == 26
+};
